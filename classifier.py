c6c737b0b94f14f595654d4714da7d3c6e71c145
IvanYingX <ying.yingxuan@hotmail.com>
2021-04-06 00:26:02 +0100

    Normalized the input

--- classifier.py
+++ classifier.py
@@ -3,9 +3,10 @@ from PIL import Image
 import cv2
 import re
 import os
-
+# import tensorflow as tf
+import numpy as np
 # the TFLite converted to be used with edgetpu
-modelPath = 'model_unquant.tflite'
+modelPath = 'model.tflite'
 
 # The path to labels.txt that was downloaded with your model
 labelPath = 'labels.txt'
@@ -18,13 +19,28 @@ def loadLabels(labelPath):
         return {int(num): text.strip() for num, text in lines}
 
 # This function takes in a PIL Image from any source or path you choose
+def normalize(arr):
+    arr = arr.astype('float')
+    for i in range(3):
+        minval = arr[...,i].min()
+        maxval = arr[...,i].max()
+        if minval != maxval:
+            arr[...,i] -= minval
+            arr[...,i] *= (255./(maxval-minval))
+    return arr
+
+
 def classifyImage(image_path, engine):
     # Load and format your image for use with TM2 model
     # image is reformated to a square to match training
-    image = Image.open(image_path)
-    image.resize((224, 224))
-
+    # print(image_path)
+    # image = Image.open(image_path)
+    # image.resize((224, 224))
+    # image = tf.keras.preprocessing.image.img_to_array(image_path)
+    # image = tf.expand_dims(image, axis=0)
     # Classify and ouptut inference
+    image = np.array(image_path)
+    image = Image.fromarray(normalize(image).astype('uint8'), 'RGB')
     classifications = engine.ClassifyWithImage(image)
     return classifications
 
@@ -44,9 +60,8 @@ def main():
         pil_im = Image.fromarray(cv2_im)
 
         # Resize and flip image so its a square and matches training
-        pil_im.resize((224, 224))
-        pil_im.transpose(Image.FLIP_LEFT_RIGHT)
-
+        pil_im = pil_im.resize((224, 224), Image.ANTIALIAS)
+        pil_im = pil_im.transpose(Image.FLIP_LEFT_RIGHT)
         # Classify and display image
         results = classifyImage(pil_im, engine)
         cv2.imshow('frame', cv2_im)
